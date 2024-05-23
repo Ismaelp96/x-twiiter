16fdc50d548f41389e0f1185ffbda824b61a54fc
ismaelp96 <ismael6991@live.com>
2024-05-23 15:26:37 -0300

    Update postpage component to fetch data from Firestore and display post datails

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import SessionWrapper from '@/components/SessionWrapper';
+import Input from '@/components/Input';
+import Sidebar from '@/components/Sidebar';
+import News from '@/components/News';
+import CommentModal from '@/components/CommentModal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +20,23 @@ export default function RootLayout({ children }) {
     <SessionWrapper>
       <html lang='en'>
         <body className={inter.className}>
-          <div>{children}</div>
+          <div className='flex items-start justify-between w-full max-w-[1440px] mx-auto'>
+            <div className='h-screen w-full max-w-24 xl:max-w-[220px] sticky top-0'>
+              <Sidebar />
+            </div>
+            <div>{children}</div>
+            <div className='lg:flex-col p-3 min-h-screen hidden lg:flex w-full max-w-[24rem]'>
+              <div className='sticky top-0 bg-white py-2 space-y-4'>
+                <input
+                  type='text'
+                  placeholder='Search'
+                  className='bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
+                />
+              </div>
+              <News />
+            </div>
+          </div>
+          <CommentModal />
         </body>
       </html>
     </SessionWrapper>
--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,3 +1,27 @@
-export default function PostPage() {
-  return <div>PostPage</div>;
+import Post from '@/components/Post';
+import { app } from '@/firebase';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import Link from 'next/link';
+import { HiArrowLeft } from 'react-icons/hi';
+
+export default async function PostPage({ params }) {
+  const db = getFirestore(app);
+  let data = {};
+  const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+  data = { ...querySnapshot.data(), id: querySnapshot.id };
+
+  return (
+    <div className='w-full border-l min-h-screen border-r'>
+      <div className='w-full px-3 py-2 sticky top-0 z-20 bg-white border-b border-b-gray-200 flex items-center space-x-2'>
+        <Link
+          href={'/'}
+          className='hover:bg-gray-100 transition-colors duration-200 p-2 rounded-full'
+        >
+          <HiArrowLeft className='h-5 w-5' />
+        </Link>
+        <span className='text-base sm:text-lg'>Back</span>
+      </div>
+      <Post post={data} id={data.id} />
+    </div>
+  );
 }
