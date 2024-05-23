31aacb9ba179ea09f6025fa3d910106405133564
ismaelp96 <ismael6991@live.com>
2024-05-23 16:12:18 -0300

    Add Like funcionality to Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,78 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import { HiDotsHorizontal } from 'react-icons/hi';
+import { app } from '@/firebase';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { HiDotsHorizontal, HiHeart, HiOutlineHeart } from 'react-icons/hi';
+import { signIn, useSession } from 'next-auth/react';
 
-export default function Comment({ comment, id }) {
+export default function Comment({ comment, commentId, originalPostId }) {
+  const db = getFirestore(app);
+  const { data: session } = useSession();
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
+
+  const handlerLikePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(
+          doc(
+            db,
+            'posts',
+            originalPostId,
+            'comments',
+            commentId,
+            'likes',
+            session?.user.uid
+          )
+        );
+      } else {
+        await setDoc(
+          doc(
+            db,
+            'posts',
+            originalPostId,
+            'comments',
+            commentId,
+            'likes',
+            session.user.uid
+          ),
+          {
+            username: session.user.username,
+            timestamp: serverTimestamp(),
+          }
+        );
+      }
+    } else {
+      signIn();
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(
+      collection(db, 'posts', originalPostId, 'comments', commentId, 'likes'),
+      (snapshot) => {
+        setLikes(snapshot.docs);
+      }
+    );
+  }, [db, originalPostId, commentId]);
+
+  useEffect(() => {
+    setIsLiked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes, session?.user?.uid]);
+
   return (
     <div className='w-full flex items-start justify-between border-b border-gray-200 p-4 gap-2 hover:bg-gray-50 transition-colors duration-300 pl-10'>
       <div className='max-w-[40px]'>
@@ -24,6 +93,24 @@ export default function Comment({ comment, id }) {
           <HiDotsHorizontal className='text-sm' />
         </div>
         <p className='text-gray-800 text-xs my-3'>{comment?.comment}</p>
+        <div className='flex items-center'>
+          {isLiked ? (
+            <HiHeart
+              onClick={handlerLikePost}
+              className='h-9 w-9 cursor-pointer rounded-full transiton duration-500 text-red-600 ease-in-out p-2 hover:text-gray-300'
+            />
+          ) : (
+            <HiOutlineHeart
+              onClick={handlerLikePost}
+              className='h-9 w-9 cursor-pointer rounded-full transiton duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+            />
+          )}
+          {likes.length > 0 && (
+            <span className={`text-xs ${isLiked && 'text-red-600'}`}>
+              {likes.length}
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
